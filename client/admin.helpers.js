b1f84762652d6ace68eccd1be65417cd8c0d3dde
unknown <GMonne@IT-ESP04.solit-servtec.conatel.com.uy>
2015-06-22 14:04:37 -0300

    minor improvements

--- client/admin.helpers.js
+++ client/admin.helpers.js
@@ -1,5 +1,31 @@
+var count = function(){ return Clients.find({}).fetch().map(function(client){ return client.count || 0 }); };
+
+var first = function(){ 
+  if (Clients.find({}).count() === 0){ return 0; }
+  return count().reduce(function(n, m){ if (n < m) { return n } else { return m } }); };
+
+var last  = function(){ 
+  if (Clients.find({}).count() === 0){ return 0; }
+  return count().reduce(function(n, m){ if (n > m) { return n } else { return m } }); };
+
+var next  = function(){ return Counters.findOne({collection: 'clients'}).count + 1; }
+
+var resetCounter = function(){ Counters.update(clientsCounterId(), { $set: { count: 0 } }); };
+
+var clientsCounterId = function(){ return Counters.findOne({collection: 'clients'})._id; };
+
+var filter = function(event, template){ Session.set('filter', template.$('#filter').val()); };
+
+var clients = function(){
+  var query = {}, filter = Session.get('filter');
+  if (filter && filter !== ""){
+    query = {$or: [ {name: filter}, {company: filter}, {count: parseInt(filter)} ]};
+  }
+  return Clients.find(query, {sort: {count: -1}}).fetch();
+};
+
 Template.admin.helpers({
-  clients : function(){ return Clients.find({}, {sort: {count: -1}}).fetch(); },
+  clients : function(){ return clients(); },
   first   : function(){ return first(); },
   last    : function(){ return last(); },
   next    : function(){ return next(); },
@@ -21,15 +47,5 @@ Template.admin.events({
       Meteor.call('cleanClientsTable');
     }
   },
+  'keydown #filter': _.debounce(filter, 500),
 });
-
-var count = function(){ return Clients.find({}).fetch().map(function(client){ return client.count || 0 }); };
-var first = function(){ 
-  if (Clients.find({}).count() === 0){ return 0; }
-  return count().reduce(function(n, m){ if (n < m) { return n } else { return m } }); };
-var last  = function(){ 
-  if (Clients.find({}).count() === 0){ return 0; }
-  return count().reduce(function(n, m){ if (n > m) { return n } else { return m } }); };
-var next  = function(){ return Counters.findOne({collection: 'clients'}).count + 1; }
-var resetCounter = function(){ Counters.update(clientsCounterId(), { $set: { count: 0 } }); };
-var clientsCounterId = function(){ return Counters.findOne({collection: 'clients'})._id; };
